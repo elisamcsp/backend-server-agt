545996cc96c2e9c58d6d1bb519aa84f2309e3683
Elisa Méndez Cáceres <emendezcaceres@gmail.com>
2022-08-24 21:28:31 +0200

    Añadido el servicio de eliminar usuario

--- routes/user.js
+++ routes/user.js
@@ -53,6 +53,87 @@ app.post("/", (req, res) => {
   });
 });
 
+// ==========================================
+// Actualizar usuario
+// ==========================================
+app.post("/:id", (req, res) => {
+  var id = req.params.id;
+  var body = req.body;
+
+  User.findById(id, (err, user) => {
+    if (err) {
+      return res.status(500).json({
+        ok: false,
+        message: "Error al buscar el usuario",
+        errors: err,
+      });
+    }
+
+    if (!user) {
+      return res.status(400).json({
+        ok: false,
+        message: "El usuario con el id" + id + "no existe",
+        errors: {
+          message: "No existe un usuario con ese ID en la base de datos",
+        },
+      });
+    }
+
+    user.name = body.name;
+    user.email = body.email;
+    user.role = body.role;
+
+    user.save((err, userSave) => {
+      if (err) {
+        return res.status(400).json({
+          ok: false,
+          message: "Error al actualizar usuario",
+          errors: err,
+        });
+      }
+
+      userSave.password = ":)";
+
+      res.status(200).json({
+        ok: true,
+        user: userSave,
+      });
+    });
+  });
+});
+
+// ==========================================
+// Eliminar usuario por el id
+// ==========================================
+app.delete("/:id", (req, res) => {
+  var id = req.params.id;
+
+  User.findByIdAndRemove(id, (err, userDeleted) => {
+    if (err) {
+      return res.status(500).json({
+        ok: false,
+        mensaje: "Error al borrar usuario",
+        errors: err,
+      });
+    }
+
+    if (!userDeleted) {
+      return res.status(400).json({
+        ok: false,
+        message: "No existe un usuario con ese id",
+        errors: {
+          message: "No existe un usuario con ese id",
+        },
+      });
+    }
+
+    res.status(200).json({
+      ok: true,
+      user: userDeleted,
+    });
+  });
+});
+
 module.exports = app;
 
 app.get("");
